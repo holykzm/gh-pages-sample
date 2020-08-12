8d3832527576e861e91bae328a03e55458df661a
holykzm <64673947+holykzm@users.noreply.github.com>
2020-08-12 18:57:59 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
-        var date2 = $('input[name="date2"]').val();
-        var date3 = $('input[name="date3"]').val();
         var number = $('input[name="number"]:').val();
         var names = '';
         $('#form-name').children().each(function (i, elm) {
@@ -29,11 +27,11 @@ $(function () {
         })
         names = names.slice(0, -1);
         var staff = $('input[name="s-name"]:checked').val();
-        var menu = $('input[name="menu"]:checked').val();
+        var menu = $('input[name="menu"]').val();
         var memo = $('input[name="memo"]').val();
 
 
-        var msg = `希望日：${date}\n第２希望：${date2}\n第３希望：${date3}\n人数：${number}人\n氏名：${names}様\n担当指名：${staff}\nメニュー：${menu}\n概要：${memo}\n\n送信ありがとうございます！\n返信が来るまでしばらくお待ちください♪`;
+        var msg = `希望日：${date}\n人数：${number}人\n氏名：${names}様\n担当指名：${staff}\nメニュー：${menu}\n概要：${memo}\n\n送信ありがとうございます！\n返信が来るまでしばらくお待ちください♪`;
         sendText(msg);
 
         return false;
