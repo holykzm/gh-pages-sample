e3eba7ca8503ebec3e8e2d34625f464462c4667a
holykzm <64673947+holykzm@users.noreply.github.com>
2020-08-12 20:19:26 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 $(function () {
     // カレンダー
     $(function () {
-        $('input[name="date"]').datepicker({
+      $('#datetime').datetimepicker({
             dateFormat: 'yy/mm/dd hh:mm',
             language:'ja',
         });
     });
 
-    // 参加人数分の氏名欄を生成
-    $('#form-number').click(function () {
-        $('#form-name').empty();
-        var num = $('input[name="number"]:checked').val();
-        for (i = 0; i < num; i++) {
-            $('#form-name').append(
-                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
-            );
-        }
-    });
-
     // 送信
     $('form').submit(function () {
-        var date = $('input[name="date"]').val();
+        var date = $('#datetime').val();
         var names = $('input[name="names"]').val();
         var staff = $('input[name="s-name"]:checked').val();
         var menu = $('input[name="menu"]').val();
