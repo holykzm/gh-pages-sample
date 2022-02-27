17b4f384ee7710b8afb06241e8e78ff3b50b5bee
holykzm-sub <64673947+holykzm@users.noreply.github.com>
2022-02-27 17:42:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -75,12 +75,7 @@ $(function () {
         var date2 = $('#datepicker2').val();
         var time21 = $('#timepicker2-1').val();
         var time22 = $('#timepicker2-2').val();
-        
-        if ($date2 == ''){
-        var asasas = $date2+$time21+$time22;
-        } else {
-        var asasas = '';
-        }
+
         
         var date3 = $('#datepicker3').val();
         var time31 = $('#timepicker3-1').val();
@@ -96,7 +91,7 @@ $(function () {
         var memo = $('input[name="memo"]').val();
 
 
-        var msg = `希望日：${date}\n\n時間：${time}～${time2}までの間 \n\n（第2希望）\n${asasas}までの間\n\n（第３希望）\n${date3}\n${time31}～${time32}までの間\n\n氏名：${names}様\n\n 担当指名：${staff}\n\nメニュー：${menu}\n\n概要：${memo}\n\n\n送信ありがとうございます！\n返信が来るまでしばらくお待ちください♪`;
+        var msg = `希望日：${date}\n\n時間：${time}～${time2}までの間\n\n（第2希望）\n${date2}\n${time21}～${time22}までの間\n\n（第３希望）\n${date3}\n${time31}～${time32}までの間\n\n氏名：${names}様\n\n 担当指名：${staff}\n\nメニュー：${menu}\n\n概要：${memo}\n\n\n送信ありがとうございます！\n返信が来るまでしばらくお待ちください♪`;
         sendText(msg);
 
         return false;
